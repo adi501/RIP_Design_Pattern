64903ae05e2521293d9e41de85d73e1df1cdbb96
Adinarayana Reddy JC <jc.adi101@gmail.com>
2025-02-05 11:33:32 +0530

    Add project files.

---
+++ WithOut_RIP/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter your skill set for job opening (like .net,java,php,)");
+string knowledge = Console.ReadLine();
+if (knowledge == ".net")
+{
+    Console.WriteLine("Requirement matches");
+}
+else
+    if (knowledge == "java")
+{
+    Console.WriteLine("Requirement matches");
+}
+else
+    if (knowledge == "php")
+{
+    Console.WriteLine("Requirement matches");
+}
+else
+    if (knowledge == "reactjs")
+{
+    Console.WriteLine("Requirement matches");
+}
+else
+{
+    Console.WriteLine("Requirement does not matches");
+}
+Console.ReadLine();
+
