19f5b8f23c8ef0580c2804e8d03d496d11dab611
Adinarayana Reddy JC <jc.adi101@gmail.com>
2025-02-05 12:04:16 +0530

    RIP Design Pattern

---
+++ With_RIP/Program.cs
@@ -0,0 +1,23 @@
+﻿//Ref : https://www.c-sharpcorner.com/article/replace-conditional-statements-ifelse-or-switch-with-factory/
+
+Console.WriteLine("Enter your skill set for job opening (like .net,java,php,reactjs)");
+string knowledge = Console.ReadLine();
+Console.WriteLine(SimpleFactory.Create(knowledge.ToLower()));
+Console.ReadKey();
+
+public class SimpleFactory
+{
+    private static Dictionary<string, string> skill = new Dictionary<string, string>();
+    static SimpleFactory()
+    {
+        skill.Add(".net", "Requirement matches");
+        skill.Add("java", "Requirement matches");
+        skill.Add("php", "Requirement matches");
+        skill.Add("reactjs", "Requirement matches");
+    }
+    public static string Create(string CustType)
+    {
+        // Design Pattern : RIP Pattern
+        return skill[CustType];
+    }
+}
